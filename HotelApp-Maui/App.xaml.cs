c087a21f0c3e52d9bb93cbe0a11fd144faebcc34
jjpxt <jotajunior.peixoto@gmail.com>
2026-04-05 21:10:12 -0300

    Lógica para checkin e checkout

--- HotelApp-Maui/App.xaml.cs
+++ HotelApp-Maui/App.xaml.cs
@@ -1,7 +1,38 @@
-﻿namespace HotelApp_Maui
+﻿using HotelApp_Maui.Models;
+
+namespace HotelApp_Maui
 {
     public partial class App : Application
     {
+        public List<Quarto> lista_quartos = new List<Quarto>
+        {
+            new Quarto()
+            {
+                Descricao = "Suíte Super Luxo",
+                ValorDiariaAdulto = 110,
+                ValorDiariaCrianca = 55
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte Luxo",
+                ValorDiariaAdulto = 80,
+                ValorDiariaCrianca = 40
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte Single",
+                ValorDiariaAdulto = 50.0,
+                ValorDiariaCrianca = 25.0
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte Simples",
+                ValorDiariaAdulto = 30,
+                ValorDiariaCrianca = 15
+            },
+        };
+
+
         public App()
         {
             InitializeComponent();
---
+++ HotelApp-Maui/Models/Quarto.cs
@@ -0,0 +1,8 @@
+﻿namespace HotelApp_Maui.Models;
+
+public class Quarto
+{
+    public string Descricao { get; set; }
+    public double ValorDiariaAdulto { get; set; }
+    public double ValorDiariaCrianca { get; set; }
+}
